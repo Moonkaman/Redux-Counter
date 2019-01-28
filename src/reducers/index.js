73ad92017afac5c1333398976ddcb01b25c186d5
Moonkaman <nickbatmanglidj@gmail.com>
2019-01-28 13:53:58 -0700

    Fill in action creators & update reducers to reflect that

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -14,12 +14,12 @@ export default (state = initialState, action) => {
     case INCREMENT:
       return {
         ...state,
-        count: state.count++
+        count: action.payload++
       }
     case DECREMENT:
       return {
         ...state,
-        count: state.count--
+        count: action.payload--
       }
     default:
       return state;
